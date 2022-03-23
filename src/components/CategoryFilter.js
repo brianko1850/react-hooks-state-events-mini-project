6c566801658e334c37832e788c3e2bf4dd69cf0a
brianko <kobj11@gmail.com>
2022-03-23 17:18:13 -0400

    work in progress

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CategoryFilter({categories}) {
+  const[selected, setSelected]=useState(false)
+  // const selectState = selected ? "selected" : ""
+  
+
+
   return (
     <div className="categories" >
       <h5>Category filters</h5>
       {categories.map((category)=>{
+         const selectState = selected ? "selected" : ""
         return(
-          <button key={category} class="selected">{category}</button>
+          <button key={category} className={selectState} onClick={()=>setSelected(!selected)} >{category}</button>
         )
       })}
     </div>
